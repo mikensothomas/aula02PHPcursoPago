8edf676e7f5c365221f9824b7b53ecef4435f950
mikensothomas <thomasmikenson40@gmail.com>
2023-01-28 14:14:45 -0300

    c

--- send.php
+++ send.php
@@ -15,17 +15,17 @@ require 'vendor/autoload.php';
 $mail = new PHPMailer;
 $mail->isSMTP();
 $mail->SMTPDebug = 2;
-$mail->Host = 'smtp.gmail.com';
-$mail->Username = 'mikensonthomas0@gmail.com';
-$mail->Password = '17011994yeyeye';
-$mail->Port = 465;
+$mail->Host = 'smtp.zerobugs.com.br';
+$mail->Port = 587;
 $mail->SMTPAuth = true;
+$mail->Username = 'teste@zerobugs.com.br';
+$mail->Password = 'mc0BfI9*mm<';
 
 $mail->SMTPSecure = false;
 $mail->isHTML(true);
 $mail->CharSet = 'UTF-8';
 
-$mail->setFrom('mikensonthomas0@gmail.com', "Teste Zero Bugs");
+$mail->setFrom('teste@zerobugs.com.br', "Teste Zero Bugs");
 $mail->addAddress('mikensonthomas2@gmail.com');
 $mail->Subject = 'MikensonThomas';
 $mail->Body = "h1>Email enviado com sucesso</h1>";
